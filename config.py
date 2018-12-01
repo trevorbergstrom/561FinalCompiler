220b981326ae8a2ffe7ae100858861d408c43497
trevorbergstrom <trevor.bergstrom@gmail.com>
2018-12-01 13:25:03 -0800

    new Repo

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 name = Trevor Bergstrom
 host = ix.cs.uoregon.edu
 path = /home/users/bergsttr/Documents/CIS561/CompilerFinal
-repo = https://github.com/trevorbergstrom/NewCompiler
+repo = https://github.com/trevorbergstrom/561FinalCompiler
